d031728514df88407a0be990a5b74e405aca57f9
Patrick Rohr <patrickrohr92@gmail.com>
2019-01-06 22:06:21 -0500

    Let there be light!
    Setup finally seems to be working. GCLK is initialized to use DFLL48.
    LED turns on as expected. YAY!

--- Configurations/SAMD21/toolchain_samd21g18a.cmake
+++ Configurations/SAMD21/toolchain_samd21g18a.cmake
@@ -1,11 +1,13 @@
 #CMake options to cross-compile for Atmel SAMD21G18A from Linux on PC
 set(TOOLCHAIN "Target")
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nano.specs")
 set(CMAKE_C_COMPILER /usr/bin/arm-none-eabi-gcc)
 set(CMAKE_ASM_COMPILER /usr/bin/arm-none-eabi-as)
 set(CMAKE_OBJCOPY /usr/bin/arm-none-eabi-objcopy)
+set(CMAKE_BUILD_TYPE Debug)
 
-add_compile_options(-mtune=cortex-m0)
+add_compile_options(-mtune=cortex-m0plus)
 add_compile_options(-mthumb)
 add_compile_options(-march=armv6-m)
+# add_compile_options(-nostdlib)
--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Or at least that's what it once was.
 
 At this point this has become the project for all things SAMD21.
 
-Right now, this doesn't do much, but the build system is pretty darn clean.
+Right now, this doesn't do much, but the build system is (was :)) pretty darn clean.
 
 You can build the project on your system using:
 
 `make all` - Builds all configurations
 
-`make HostCI` - Builds host configuration
+`make HostCI` - Builds host configuration (Yeah, that's a dream. But maybe useful to just build libs for a unit test environment?)
 
 `make TargetDebug` - Builds target debug configuration
 
