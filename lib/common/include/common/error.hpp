10560e84c72566d90f42f4a4fca430d40532e988
Patrick Rohr <patrickrohr92@gmail.com>
2019-07-16 17:25:24 +0200

    Adding (VERY) basic C++ Error Definitions

---
+++ lib/common/include/common/error.hpp
@@ -0,0 +1,28 @@
+
+#pragma once
+
+#include "common/environment.hpp"
+
+namespace SAMD
+{
+
+using error_t = int;
+
+template<Configuration CONFIG = eRuntimeConfiguration>
+struct assert
+{
+    // add error code
+    constexpr assert(bool statement)
+    {
+        // infinite while loop when assertion is false.
+        while (!statement) {}
+    }
+};
+
+template<>
+struct assert<Configuration::eRelease>
+{
+    constexpr assert(bool statement) { (void)statement; }
+};
+
+} // namespace SAMD
