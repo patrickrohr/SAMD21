8bae7a253abebbe50e61e7254332c3c21098d714
Patrick Rohr <patrickrohr92@gmail.com>
2019-07-23 17:14:52 +0200

    Adding ID Traits

---
+++ lib/common/include/common/id_traits.hpp
@@ -0,0 +1,46 @@
+/**************************************************************
+ *                                                            *
+ *               Copyright (c) 2019 - Patrick Rohr            *
+ *                      All Rights Reserved                   *
+ *                                                            *
+ *************************************************************/
+
+#pragma once
+
+namespace SAMD
+{
+
+template<typename CLASS, typename TYPE = unsigned>
+class id_traits
+{
+public:
+    using type = TYPE;
+
+public:
+    constexpr id_traits() : m_id()
+    {
+    }
+    constexpr explicit id_traits(type id) : m_id(id)
+    {
+    }
+
+    constexpr bool operator==(id_traits rhs) const
+    {
+        return m_id == rhs.m_id;
+    }
+
+    type Get() const
+    {
+        return m_id;
+    }
+
+    explicit operator type() const
+    {
+        return m_id;
+    }
+
+private:
+    type m_id;
+};
+
+} // namespace SAMD
