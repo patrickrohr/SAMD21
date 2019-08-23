e14209dd62109ca8465f0b5094edc8d14ec11481
Patrick Rohr <patrickrohr92@gmail.com>
2019-08-23 08:46:58 +0200

    Adding Basic Array Functionality Supporting SequenceIterators

---
+++ lib/common/include/common/array.hpp
@@ -0,0 +1,86 @@
+/**************************************************************
+ *                                                            *
+ *               Copyright (c) 2019 - Patrick Rohr            *
+ *                      All Rights Reserved                   *
+ *                                                            *
+ *************************************************************/
+
+#pragma once
+
+#include "sequence_iterator.hpp"
+
+namespace SAMD
+{
+
+template<typename T, unsigned SIZE>
+class Array
+{
+public:
+    static constexpr unsigned SizeAllocated = SIZE;
+
+    using element_type  = T;
+    using iterator_type = SequenceIterator<Array<T, SIZE>>;
+    using index_type    = unsigned;
+
+private:
+    friend iterator_type;
+
+public:
+    Array() : m_arrData()
+    {
+    }
+
+    T* Data()
+    {
+        return m_arrData[0];
+    }
+
+    T& operator[](const index_type& index)
+    {
+        samd_assert(index < SIZE, "Array Index Overflow");
+        return m_arrData[index];
+    }
+
+    unsigned Size()
+    {
+        return SizeAllocated;
+    }
+
+    iterator_type Begin()
+    {
+        return iterator_type(this, 0);
+    }
+
+    iterator_type End()
+    {
+        return iterator_type(this, SIZE);
+    }
+
+private:
+    void Next(iterator_type& it)
+    {
+        ++it.m_index;
+    }
+
+    void Prev(iterator_type& it)
+    {
+        --it.m_index;
+    }
+
+private:
+    T m_arrData[SIZE];
+};
+
+template<typename T, unsigned SIZE>
+typename Array<T, SIZE>::iterator_type begin(Array<T, SIZE>& arr)
+{
+    return arr.Begin();
+}
+
+template<typename T, unsigned SIZE>
+typename Array<T, SIZE>::iterator_type end(Array<T, SIZE>& arr)
+{
+    return arr.End();
+}
+
+} // namespace SAMD
---
+++ lib/common/tests/src/array_test.cpp
@@ -0,0 +1,32 @@
+/**************************************************************
+ *                                                            *
+ *               Copyright (c) 2019 - Patrick Rohr            *
+ *                      All Rights Reserved                   *
+ *                                                            *
+ *************************************************************/
+
+#include <gtest/gtest.h>
+#include "common/array.hpp"
+
+using namespace SAMD;
+
+TEST(ArrayTest, Test)
+{
+    Array<int, 3> arr;
+
+    arr[0] = 1;
+    arr[1] = 15;
+    arr[2] = 17;
+
+    ASSERT_EQ(arr[0], 1);
+    ASSERT_EQ(arr[1], 15);
+    ASSERT_EQ(arr[2], 17);
+
+    unsigned count = 0;
+    for (const auto& item : arr)
+    {
+        ++count;
+    }
+
+    ASSERT_EQ(arr.Size(), count);
+}
