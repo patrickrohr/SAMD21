a9587072189ce1c9d62d9e88b36367126f57dbfd
Patrick Rohr <patrickrohr92@gmail.com>
2019-08-28 07:08:34 +0200

    Adding ConstIterator Support to Vector

--- lib/common/include/common/vector.hpp
+++ lib/common/include/common/vector.hpp
@@ -7,7 +7,8 @@
 
 #pragma once
 
-#include "sequence_iterator.hpp"
+#include "common/array.hpp"
+#include "common/sequence_iterator.hpp"
 
 namespace SAMD
 {
@@ -20,6 +21,7 @@ public:
 
     using element_type  = T;
     using iterator_type = SequenceIterator<Vector<T, SIZE>>;
+    using const_iterator_type = SequenceIterator<const Vector<T, SIZE>>;
     using index_type    = unsigned;
 
 private:
@@ -32,13 +34,18 @@ public:
 
     T* Data()
     {
-        return m_arrData[0];
+        return m_arrData.Data();
     }
 
     T& operator[](const index_type& index)
     {
         samd_assert(index < m_uSizeUsed, "Vector Index Overflow");
         return m_arrData[index];
+    }
+
+    const T& operator[](const index_type& index) const
+    {
+        return const_cast<Vector<T, SIZE>*>(this)->operator[](index);
     }
 
     void Resize(unsigned newSize)
@@ -50,10 +57,7 @@ public:
             // de-allocate objects, is there a better option than replacing with
             // a default-constructed object?
             for (unsigned i = m_uSizeUsed; i > newSize; --i)
-            {
-                m_arrData[i] = T();
-            }
-        }
+            { m_arrData[i] = T(); } }
         m_uSizeUsed = newSize;
     }
 
@@ -84,9 +88,19 @@ public:
         return iterator_type(this, 0);
     }
 
+    const_iterator_type ConstBegin() const
+    {
+        return const_iterator_type(this, 0);
+    }
+
     iterator_type End()
     {
         return iterator_type(this, m_uSizeUsed);
+    }
+
+    const_iterator_type ConstEnd()
+    {
+        return const_iterator_type(this, m_uSizeUsed);
     }
 
 private:
@@ -101,7 +115,7 @@ private:
     }
 
 private:
-    T m_arrData[SIZE];
+    Array<T, SIZE> m_arrData;
     unsigned m_uSizeUsed;
 };
 
@@ -115,6 +129,18 @@ template<typename T, unsigned SIZE>
 typename Vector<T, SIZE>::iterator_type end(Vector<T, SIZE>& vec)
 {
     return vec.End();
+}
+
+template<typename T, unsigned SIZE>
+typename Vector<T, SIZE>::iterator_type begin(const Vector<T, SIZE>& vec)
+{
+    return vec.ConstBegin();
+}
+
+template<typename T, unsigned SIZE>
+typename Vector<T, SIZE>::iterator_type end(const Vector<T, SIZE>& vec)
+{
+    return vec.ConstEnd();
 }
 
 } // namespace SAMD
